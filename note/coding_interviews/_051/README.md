363910bbb02d624fa3b86c78f8b4bf80af06be88
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2020-12-06 20:47:09 +0800

    reverse pairs

---
+++ note/coding_interviews/_051/README.md
@@ -0,0 +1,10 @@
+# [数组中的逆序对][title]
+
+## Solution
+https://nightxlt.github.io/2019/03/02/%E6%95%B0%E7%BB%84%E4%B8%AD%E7%9A%84%E9%80%86%E5%BA%8F%E5%AF%B9/
+
+## Conclusion
+如果你同我一样热爱数据结构、算法、LeetCode，可以关注我 GitHub 上的 LeetCode 题解：[awesome-kotlin-leetcode][akl]
+
+[title]: https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/
+[akl]: https://github.com/NightXlt/awesome-kotlin-leetcode
---
+++ src/com/blankj/coding_interviews/_51/Solution.kt
@@ -0,0 +1,53 @@
+package com.blankj.coding_interviews._51
+
+import com.blankj.coding_interviews._004.print
+
+
+class Solution {
+
+    fun reversePairs(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+        val copy = IntArray(nums.size)
+        return mergeSort(nums, copy, 0, nums.size - 1)
+    }
+
+    private fun mergeSort(nums: IntArray, copy: IntArray, low: Int, high: Int): Int {
+        if (low >= high) return 0
+        val mid = (low + high) shr 1
+        val leftReversePairs = mergeSort(nums, copy, low, mid)
+        val rightReversePairs = mergeSort(nums, copy, mid + 1, high)
+        val midReversePairs = merge(nums, copy, low, mid, high)
+        return leftReversePairs + rightReversePairs + midReversePairs
+    }
+
+    private fun merge(nums: IntArray, copy: IntArray, low: Int, mid: Int, high: Int): Int {
+        for (i in low..high) {
+            copy[i] = nums[i]
+        }
+        var i = mid
+        var j = high
+        var k = high
+        var count = 0
+        while (i >= low && j > mid) {
+            if (copy[i] > copy[j]) {
+                count += j - mid
+                nums[k--] = copy[i--]
+            } else {
+                nums[k--] = copy[j--]
+            }
+        }
+        while (i >= low) {
+            nums[k--] = copy[i--]
+        }
+        while (j > mid) {
+            nums[k--] = copy[j--]
+        }
+        return count
+    }
+}
+
+fun main() {
+    Solution().reversePairs(intArrayOf(7, 5, 6, 4)).print()
+    Solution().reversePairs(intArrayOf(7, 6, 5, 4)).print()
+    Solution().reversePairs(intArrayOf(1, 2, 3, 4)).print()
+}
