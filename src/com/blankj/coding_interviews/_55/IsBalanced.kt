1c529baa1f4e794b37cef6c6b089c9249e37b338
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2020-12-08 17:24:07 +0800

    bitree max depth

---
+++ src/com/blankj/coding_interviews/_55/IsBalanced.kt
@@ -0,0 +1,29 @@
+package com.blankj.coding_interviews._55
+
+import com.blankj.coding_interviews._004.print
+import com.blankj.structure.TreeNode
+import kotlin.math.abs
+import kotlin.math.max
+
+// https://leetcode-cn.com/problems/balanced-binary-tree/submissions/
+class IsBalanced {
+    fun isBalanced(root: TreeNode?): Boolean {
+        return if (root == null) {
+            true
+        } else isBalanced(root, 1) != -1
+    }
+
+    private fun isBalanced(root: TreeNode?, depth: Int): Int {
+        root ?: return 0
+        val leftDepth = isBalanced(root.left, depth + 1)
+        if (leftDepth == -1) return -1
+        val rightDepth = isBalanced(root.right, depth + 1)
+        if (rightDepth == -1) return -1
+        val diff = abs(leftDepth - rightDepth)
+        return if (diff <= 1) max(leftDepth, rightDepth) + 1 else -1
+    }
+}
+
+fun main() {
+    IsBalanced().isBalanced(TreeNode.createTestData("[1,2,2,3,3,null,null,4,4]")).print()
+}
---
+++ src/com/blankj/coding_interviews/_55/Solution.kt
@@ -0,0 +1,24 @@
+package com.blankj.coding_interviews._55
+
+import com.blankj.structure.TreeNode
+import kotlin.math.max
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        return if (root == null) {
+            0
+        } else max(maxDepth(root.left), maxDepth(root.right)) + 1
+    }
+}
