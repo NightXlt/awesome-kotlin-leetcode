acd3267eebebdcd142836ef365c5af75f9c9b35d
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2020-12-25 21:09:25 +0800

    length of longest substring

---
+++ src/com/blankj/medium/_003/LengthOfLongestSubstring.kt
@@ -0,0 +1,31 @@
+package com.blankj.medium._003
+
+import com.blankj.coding_interviews._004.print
+
+class LengthOfLongestSubstring {
+
+    fun lengthOfLongestSubstring(s: String): Int {
+        if (s.isEmpty()) return 0
+        val hashMap = IntArray(128)
+        var startIndex = 0
+        var max = 0
+        for ((index, c) in s.withIndex()) {
+            val cInt = c.toInt()
+            val nearestPreCIndex = hashMap[cInt]
+            if (nearestPreCIndex >= startIndex) {
+                startIndex = nearestPreCIndex
+            }
+            val l = index - startIndex + 1
+            hashMap[cInt] = index + 1
+            if (l > max) max = l
+        }
+        return max
+    }
+}
+
+fun main() {
+    LengthOfLongestSubstring().lengthOfLongestSubstring("abcabcbb").print()
+    LengthOfLongestSubstring().lengthOfLongestSubstring("bbbbb").print()
+    LengthOfLongestSubstring().lengthOfLongestSubstring("pwwkew").print()
+    LengthOfLongestSubstring().lengthOfLongestSubstring("Abcabcbb").print()
+}
