f600c473f2bc5e6b8009449ef53880bf033f7aff
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-04-14 16:42:51 +0800

    reverse words

--- src/com/blankj/coding_interviews/_58/Solution.kt
+++ src/com/blankj/coding_interviews/_58/Solution.kt
@@ -3,7 +3,7 @@ package com.blankj.coding_interviews._58
 import com.blankj.coding_interviews._004.print
 
 class Solution {
-    fun reverseWords(s: String?): String {
+    fun reverseWords(s: String): String {
         if (s.isNullOrEmpty()) return ""
         val strings: List<String> = s.split(" ")//匹配
         val sb = StringBuilder()
@@ -16,11 +16,65 @@ class Solution {
         return sb.toString().trim()
     }
 
+    fun reverseWordsNative(s: String?): String {
+        if (s.isNullOrEmpty()) return ""
+        val result = trimSpace(s)
+        reverse(result, 0, result.length - 1)
+        reverseEachWords(result)
+        return result.toString()
+    }
+
+    private fun trimSpace(s: String): StringBuilder {
+        val stringBuilder = StringBuilder()
+        var left = 0
+        var right = s.length - 1
+        while (left <= right && s[left] == ' ') {
+            left++
+        }
+        while (left <= right && s[right] == ' ') {
+            right--
+        }
+        while (left <= right) {
+            val c = s[left]
+            if (c != ' ') {
+                stringBuilder.append(c)
+            } else if (stringBuilder.last() != ' ') {
+                stringBuilder.append(' ')
+            }
+            left++
+        }
+        return stringBuilder
+    }
+
+    private fun reverse(stringBuilder: StringBuilder, left: Int, right: Int) {
+        var left = left
+        var right = right
+        while (left < right) {
+            val leftChar = stringBuilder[left]
+            stringBuilder[left++] = stringBuilder[right]
+            stringBuilder[right--] = leftChar
+        }
+    }
+
+    private fun reverseEachWords(stringBuilder: StringBuilder) {
+        val l = stringBuilder.length
+        var wordStart = 0
+        var wordEnd = 0
+        while (wordStart < l) {
+            while (wordEnd < l && stringBuilder[wordEnd] != ' ') wordEnd++
+            reverse(stringBuilder, wordStart, wordEnd - 1)
+            wordStart = ++wordEnd
+        }
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            Solution().reverseWords("the sky is blue").print()
-            Solution().reverseWords("  hello world!  ").print()
+            Solution().reverseWordsNative("the sky is blue").print()
+            Solution().reverseWordsNative("  hello world!  ").print()
+            Solution().reverseWordsNative("     ").print()
+            Solution().reverseWordsNative("").print()
+            Solution().reverseWordsNative(null).print()
         }
     }
 }
---
+++ src/com/blankj/coding_interviews/_58_2/Solution.kt
@@ -0,0 +1,37 @@
+package com.blankj.coding_interviews._58_2
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+
+    fun reverseLeftWords(s: String?, n: Int): String {
+        if (n < 0) throw IllegalArgumentException("n must be 0 or greater")
+        if (s.isNullOrEmpty()) return ""
+        val k = n % s.length
+        // 右旋转则用下面的枢轴
+//        val partition = s.length - k
+        val result = StringBuilder(s)
+        reverse(result, 0, k - 1)
+        reverse(result, k, result.length - 1)
+        reverse(result, 0, result.length - 1)
+        return result.toString()
+    }
+
+    private fun reverse(stringBuilder: StringBuilder, left: Int, right: Int) {
+        var left = left
+        var right = right
+        while (left < right) {
+            val leftChar = stringBuilder[left]
+            stringBuilder[left++] = stringBuilder[right]
+            stringBuilder[right--] = leftChar
+        }
+    }
+}
+
+fun main() {
+    Solution().reverseLeftWords("the sky is blue", 2).print()
+    Solution().reverseLeftWords("  hello world!  ", 3).print()
+    Solution().reverseLeftWords("     ", 4).print()
+    Solution().reverseLeftWords("", 0).print()
+    Solution().reverseLeftWords(null, 2).print()
+}
