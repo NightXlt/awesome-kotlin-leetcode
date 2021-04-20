008ce066e4cf77415ed2f0f25458437060e0d14b
night <nightfeng26@gmail.com>
2021-04-20 21:38:21 +0800

    Joseph circle

---
+++ src/com/blankj/coding_interviews/_62/Solution.kt
@@ -0,0 +1,20 @@
+package com.blankj.coding_interviews._62
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+    fun lastRemaining(n: Int, m: Int): Int {
+        if (n < 1 || m < 1) return -1
+        var result = 0
+        for (i in 2..n) {
+            result = (result + m) % i
+        }
+        return result
+    }
+
+}
+
+fun main() {
+    Solution().lastRemaining(5, 3).print()
+    Solution().lastRemaining(10, 17).print()
+}
