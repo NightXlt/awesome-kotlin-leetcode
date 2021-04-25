e1813e5a992882c5ec6176c4976231b493c211d8
night <nightfeng26@gmail.com>
2021-04-25 22:45:19 +0800

    max profit

--- src/com/blankj/coding_interviews/_63/Solution.kt
+++ src/com/blankj/coding_interviews/_63/Solution.kt
@@ -1,30 +1,28 @@
 package com.blankj.coding_interviews._63
 
 import com.blankj.coding_interviews._004.print
-import java.io.File
+import kotlin.math.max
+import kotlin.math.min
 
 class Solution {
-    fun add(a: Int, b: Int): Int {
-        var sum: Int
-        var carry: Int
-        var a = a
-        var b = b
-        do {
-            sum = a xor b
-            carry = (a and b) shl 1
-            a = sum
-            b = carry
-        } while (b != 0)
-        return sum
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.size <= 1) return 0
+        var maxProfit = 0
+        // 留意这里的最值不要写反了
+        var min = Int.MAX_VALUE
+        for (price in prices) {
+            min = min(price, min)
+            maxProfit = max(maxProfit, price - min)
+        }
+        return maxProfit
     }
-
 }
 
 fun main() {
-    Solution().add(3, 0).print()
-    Solution().add(5, 3).print()
-    Solution().add(10, 17).print()
-    Solution().add(10, 10).print()
-    Solution().add(0, 0).print()
-    Solution().add(4000, 997).print()
+    Solution().maxProfit(intArrayOf(7,1,5,3,6,4)).print()
+    Solution().maxProfit(intArrayOf(7,6,4,3,1)).print()
+    Solution().maxProfit(intArrayOf(10, 17)).print()
+    Solution().maxProfit(intArrayOf(10, 10)).print()
+    Solution().maxProfit(intArrayOf(0, 0)).print()
+    Solution().maxProfit(intArrayOf(4000, 997)).print()
 }
