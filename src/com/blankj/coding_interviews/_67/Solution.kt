89fbba6bab05a0ed21e3e2529e7ef28fd319bf25
night <nightfeng26@gmail.com>
2021-04-25 22:45:44 +0800

    str to int

---
+++ src/com/blankj/coding_interviews/_67/Solution.kt
@@ -0,0 +1,63 @@
+package com.blankj.coding_interviews._67
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+
+    fun strToInt(str: String?): Int {
+        var str = str ?: return 0
+        str = str.trim()
+        val len = str.length
+        if (str.isEmpty()) {
+            return 0
+        }
+        var c = str[0]
+        var flag = 1
+        var digit = 0L
+        var digitLen = 0
+        when {
+            c == '-' -> {
+                flag = -1
+            }
+            c == '+' -> {
+                flag = 1
+            }
+            Character.isDigit(c) -> {
+                digit = (c - '0').toLong()
+            }
+            // pre char is non-digit char, so return directly
+            else -> {
+                return 0
+            }
+        }
+        for (i in 1 until len) {
+            c = str[i]
+            if (!Character.isDigit(c)) {
+                break
+            } else if (digit == 0L && c == '0') {
+                continue
+            }
+            digit = digit * 10 + c.toLong() - '0'.toLong()
+            digitLen++
+            // digit is more than Int.MAX_VALUE length
+            if (digitLen > 11) {
+                break
+            }
+        }
+        digit *= flag.toLong()
+        if (digit > Int.MAX_VALUE) {
+            return Int.MAX_VALUE
+        } else if (digit < Int.MIN_VALUE) {
+            return Int.MIN_VALUE
+        }
+        return digit.toInt()
+    }
+}
+
+fun main() {
+    Solution().strToInt("").print()
+    Solution().strToInt(null).print()
+    Solution().strToInt("null 42").print()
+    Solution().strToInt("42").print()
+    Solution().strToInt("   -42").print()
+}
