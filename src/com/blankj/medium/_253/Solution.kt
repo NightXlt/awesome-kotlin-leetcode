17553358f3eb56718728b20244d84923df4dfec0
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-05-04 21:11:14 +0800

    add comments

--- src/com/blankj/medium/_253/Solution.kt
+++ src/com/blankj/medium/_253/Solution.kt
@@ -5,14 +5,18 @@ import java.util.*
 class Solution {
 
     fun minMeetingRooms(intervals: Array<IntArray>): Int {
+        // incremental queue
         val q = PriorityQueue<Int> { a, b -> a - b }
+        // sorted by start time
         Arrays.sort(intervals) { l, r -> l[0] - r[0] }
         var res = 0
         for (i in intervals) {
+            // reuse last meeting room
             while (!q.isEmpty() && q.peek() <= i[0]) {
                 q.poll()
             }
             q.add(i[1])
+            // record max meeting room count
             if (q.size > res) {
                 res = q.size
             }
