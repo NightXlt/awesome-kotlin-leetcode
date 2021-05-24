5e8855fd9a7f865cfcb83589fe3112a6cbc856a4
night <nightfeng26@gmail.com>
2021-05-24 22:28:33 +0800

    26 numeral

---
+++ src/com/blankj/easy/_171/Solution.kt
@@ -0,0 +1,22 @@
+package com.blankj.easy._171
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+    fun titleToNumber(columnTitle: String): Int {
+        if (columnTitle.isEmpty()) return 0
+        var res= 0
+        for (c in columnTitle) {
+            val num = c - 'A' + 1
+            res = res * 26 + num
+        }
+        return res
+    }
+}
+
+fun main() {
+    Solution().titleToNumber("").print()
+    Solution().titleToNumber("AB").print()
+    Solution().titleToNumber("A").print()
+    Solution().titleToNumber("ZY").print()
+}
