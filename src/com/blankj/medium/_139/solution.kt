1e228fa651a7d5caadbc81de36b4ca1b207088bf
night <nightfeng26@gmail.com>
2021-05-24 22:27:42 +0800

    opt code

--- src/com/blankj/medium/_139/solution.kt
+++ src/com/blankj/medium/_139/solution.kt
@@ -8,9 +8,12 @@ class Solution {
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
         val dp = Array(s.length + 1) { false }
         dp[0] = true
+
+        // Use set to make sure get element is O(1)
+        val set = wordDict.toSet()
         for (i in 1..s.length)
             for (j in 0 until i)
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
                     dp[i] = true
                     break
                 }
