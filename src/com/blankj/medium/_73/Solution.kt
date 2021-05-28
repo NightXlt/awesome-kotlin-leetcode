f3145755417ffb54c8a8bb8e341d0b2cd2910095
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-05-28 16:54:14 +0800

    Set Matrix Zeroes

---
+++ src/com/blankj/medium/_73/Solution.kt
@@ -0,0 +1,45 @@
+package com.blankj.medium._73
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+    fun setZeroes(matrix: Array<IntArray>): Unit {
+        if (matrix.isEmpty() || matrix[0].isEmpty()) return
+        val rowsZero = BooleanArray(matrix.size)
+        val colsZero = BooleanArray(matrix[0].size)
+        for (i in matrix.indices) {
+            for (j in matrix[0].indices) {
+                if (matrix[i][j] == 0) {
+                    rowsZero[i] = true
+                    colsZero[j] = true
+                }
+            }
+        }
+        for (i in matrix.indices) {
+            for (j in matrix[0].indices) {
+                if (rowsZero[i] || colsZero[j]) {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+    }
+
+}
+
+fun main() {
+
+    var matrix = arrayOf(
+            intArrayOf(1, 1, 1),
+            intArrayOf(1, 0, 1),
+            intArrayOf(1, 1, 1)
+    )
+    Solution().setZeroes(matrix)
+    matrix.print()
+    matrix = arrayOf(
+            intArrayOf(0, 1, 2, 0),
+            intArrayOf(3, 4, 5, 2),
+            intArrayOf(1, 3, 1, 5)
+    )
+    Solution().setZeroes(matrix)
+    matrix.print()
+}
