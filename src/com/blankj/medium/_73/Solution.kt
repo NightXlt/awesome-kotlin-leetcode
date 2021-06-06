95f17fe032e6e169d8cc2b57a623214bc34539a0
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-06-06 18:37:39 +0800

    merge setZeroes two solution

--- src/com/blankj/medium/_73/Solution.kt
+++ src/com/blankj/medium/_73/Solution.kt
@@ -24,6 +24,45 @@ class Solution {
         }
     }
 
+    fun setZeroesWithConstantSpaceConsuming(matrix: Array<IntArray>): Unit {
+        if (matrix.isEmpty() || matrix[0].isEmpty()) return
+        var flagRowZero = false
+        var flagColZero = false
+        flagRowZero = matrix[0].contains(0)
+        matrix.forEach {
+            if (it[0] == 0) {
+                flagColZero = true
+            }
+        }
+        for (i in 1..matrix.lastIndex) {
+            for (j in 1..matrix[0].lastIndex) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                }
+            }
+        }
+        for (i in 1..matrix.lastIndex) {
+            for (j in 1..matrix[0].lastIndex) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+
+        if (flagRowZero) {
+            for (i in matrix[0].indices) {
+                matrix[0][i] = 0
+            }
+        }
+        if (flagColZero) {
+            for (i in matrix.indices) {
+                matrix[i][0] = 0
+            }
+        }
+
+    }
+
 }
 
 fun main() {
