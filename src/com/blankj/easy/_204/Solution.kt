325b3a1d38034348973b1a1fa49a6b7c2d88de3f
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-06-11 18:35:54 +0800

    count primes

---
+++ src/com/blankj/easy/_204/Solution.kt
@@ -0,0 +1,27 @@
+package com.blankj.easy._204
+
+import com.blankj.coding_interviews._004.print
+
+class Solution {
+    fun countPrimes(n: Int): Int {
+        val isPrime = BooleanArray(n) { true }
+        var res = 0
+        for (i in 2 until n) {
+            if (isPrime[i]) {
+                res += 1
+                var j = i.toLong() * i
+                while (j < n) {
+                    isPrime[j.toInt()] = false
+                    j += i
+                }
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    Solution().countPrimes(2).print()
+    Solution().countPrimes(10).print()
+    Solution().countPrimes(1).print()
+}
