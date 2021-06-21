685cc962e487b20aec412c69e5028f818fe8a87c
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-06-21 10:56:23 +0800

    51. add test case

--- src/com/blankj/coding_interviews/_51/Solution.kt
+++ src/com/blankj/coding_interviews/_51/Solution.kt
@@ -5,15 +5,15 @@ import com.blankj.coding_interviews._004.print
 
 class Solution {
 
-    fun reversePairs(nums: IntArray): Int {
-        if (nums.isEmpty()) return 0
+    fun reversePairs(nums: IntArray?): Int {
+        if (nums == null || nums.isNotEmpty()) return 0
         val copy = IntArray(nums.size)
         return mergeSort(nums, copy, 0, nums.size - 1)
     }
 
     private fun mergeSort(nums: IntArray, copy: IntArray, low: Int, high: Int): Int {
         if (low >= high) return 0
-        val mid = (low + high) shr 1
+        val mid = (low + high) shr 1 // 留意数组长度，其中 low + high是否可能溢出
         val leftReversePairs = mergeSort(nums, copy, low, mid)
         val rightReversePairs = mergeSort(nums, copy, mid + 1, high)
         val midReversePairs = merge(nums, copy, low, mid, high)
@@ -47,7 +47,11 @@ class Solution {
 }
 
 fun main() {
+    Solution().reversePairs(null).print()
+    Solution().reversePairs(intArrayOf(1)).print()
+    Solution().reversePairs(intArrayOf(2, 1)).print()
     Solution().reversePairs(intArrayOf(7, 5, 6, 4)).print()
+    Solution().reversePairs(intArrayOf(7, 7, 6, 4)).print()
     Solution().reversePairs(intArrayOf(7, 6, 5, 4)).print()
     Solution().reversePairs(intArrayOf(1, 2, 3, 4)).print()
 }
