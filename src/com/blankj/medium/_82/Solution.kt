4cdbcf15cd1f9979af1d2b9561ebf5676093ba03
xiaolingtong.night <xiaolingtong.night@bytedance.com>
2021-06-25 11:15:35 +0800

    delete duplicate

---
+++ src/com/blankj/medium/_82/Solution.kt
@@ -0,0 +1,33 @@
+package com.blankj.medium._82
+
+import com.blankj.coding_interviews._004.print
+import com.blankj.structure.ListNode
+
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        head ?: return null
+        val dummyHead = ListNode(0).apply {
+            next = head
+        }
+        var p = dummyHead
+        while (p.next != null && p.next.next != null) {
+            if (p.next.`val` == p.next?.next?.`val`) {
+                val x = p.next.`val`
+                while (p.next != null && p.next.`val` == x) {
+                    p.next = p.next?.next
+                }
+            } else {
+                p = p.next
+            }
+        }
+        return dummyHead.next
+    }
+}
+
+fun main() {
+    Solution().deleteDuplicates(null)?.print()
+    Solution().deleteDuplicates(ListNode.testCase0())?.print()
+    Solution().deleteDuplicates(ListNode.testCase1())?.print()
+    Solution().deleteDuplicates(ListNode.testCase2())?.print()
+    Solution().deleteDuplicates(ListNode.testCase3())?.print()
+}
