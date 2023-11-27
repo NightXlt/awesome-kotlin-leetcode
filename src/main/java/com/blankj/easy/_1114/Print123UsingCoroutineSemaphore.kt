38f9f74e5443da1c51a127203ce66153231faa11
xiaolingtong@bilibili.com <xiaolingtong@bilibili.com>
2023-11-27 17:30:53 +0800

    add runblocking

--- src/main/java/com/blankj/easy/_1114/Print123UsingCoroutineSemaphore.kt
+++ src/main/java/com/blankj/easy/_1114/Print123UsingCoroutineSemaphore.kt
@@ -3,7 +3,7 @@ package com.blankj.easy._1114
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
 
-suspend fun main(args: Array<String>) {
+suspend fun main(args: Array<String>) = runBlocking {
     // 定义一个信号量，初始值为 1
     val mutexA = Mutex(false)
     val mutexB = Mutex(true)
@@ -20,9 +20,7 @@ suspend fun main(args: Array<String>) {
         print("3", mutexC, mutexA)
 
     }
-    job2.join()
-    job1.join()
-    job3.join()
+    delay(30000L)
 }
 
 private suspend fun print(content: String, current: Mutex, next: Mutex) {
