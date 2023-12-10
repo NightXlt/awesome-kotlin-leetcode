924381e41b35315794165efa297a1afe969da7f1
xiaolingtong@bilibili.com <xiaolingtong@bilibili.com>
2023-12-10 16:55:04 +0800

    optimize logic

--- src/main/java/com/blankj/medium/_146/LRUCacheII.kt
+++ src/main/java/com/blankj/medium/_146/LRUCacheII.kt
@@ -3,10 +3,7 @@ package com.blankj.medium._146
 class LRUCacheII(val capacity: Int) {
     val cachedMap = object : LinkedHashMap<Int, Int>(capacity, 0.75f, true) {
         override fun removeEldestEntry(eldest: MutableMap.MutableEntry<Int, Int>?): Boolean {
-            if (capacity + 1 == this.size) {
-                return true
-            }
-            return false
+            return capacity + 1 == this.size
         }
     }
     fun get(key: Int): Int {
