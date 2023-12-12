b9753e58ab257f603309005218dd1e8c2fecd55d
xiaolingtong@bilibili.com <xiaolingtong@bilibili.com>
2023-12-12 23:05:51 +0800

    story book with map

--- src/main/java/com/blankj/duolingo/StoryBook.kt
+++ src/main/java/com/blankj/duolingo/StoryBook.kt
@@ -3,9 +3,10 @@ package com.blankj.duolingo
 import com.blankj.ext.print
 
 class StoryBook {
-    var visited = mutableSetOf<String>()
+
+    var visited = mutableMapOf<Int, MutableSet<Int>>()
     var res = mutableListOf<List<Int>>()
-    var choice = mapOf<Int, List<Int>>()
+    private var choice = mapOf<Int, List<Int>>()
 
     fun getAllPages(endings: Set<Int>, choice: List<List<Int>>): Any {
         if (endings.isEmpty()) error("endings set is empty!")
@@ -13,14 +14,13 @@ class StoryBook {
         this.choice = choice.associate {
             it[0] to it.takeLast(it.size - 1)
         }
-        visited = mutableSetOf()
+        visited = mutableMapOf()
         res = mutableListOf()
         dfs(1, stack, endings)
         if (res.isEmpty()) return -1
         val entry = res.flatten().groupBy { it }
             .maxBy { it.value.size }
         return entry.key to entry.value.size
-
     }
 
     private fun dfs(
@@ -40,16 +40,18 @@ class StoryBook {
         }
         for (next in choice.getValue(start)) {
             if (next == stack.last()) continue
-            if ("${start}_$next" in visited) continue
-            visited.add("${start}_$next")
+            val curChoiceVisited = visited.getOrPut(start) { mutableSetOf() }
+            if (next in curChoiceVisited) continue
+            visited.getValue(start).add(next)
             dfs(next, stack, endings)
             stack.removeLast()
-            visited.remove("${start}_$next")
+            visited.getValue(start).remove(next)
         }
     }
 }
 
 fun main() {
+    val start = System.currentTimeMillis()
     StoryBook().getAllPages(
         setOf(5, 10),
         listOf(
@@ -57,4 +59,5 @@ fun main() {
             listOf(9, 10, 8)
         )
     ).print()
+    println(System.currentTimeMillis() - start)
 }
