06db604b453992230f75653f6721c7026f66f7e0
xiaolingtong@bilibili.com <xiaolingtong@bilibili.com>
2023-12-15 09:49:23 +0800

    fix: logic issue

--- src/main/java/com/blankj/easy/_252/Solution.kt
+++ src/main/java/com/blankj/easy/_252/Solution.kt
@@ -7,7 +7,7 @@ class Solution {
             i1[0] - i2[0]
         }
         for (i in 0..<intervals.lastIndex) {
-            if (intervals[i + 1][0] < intervals[i][1] || intervals[i + 1][1] <= intervals[i][1]) {
+            if (intervals[i + 1][0] < intervals[i][1]) {
                 return false
             }
         }
