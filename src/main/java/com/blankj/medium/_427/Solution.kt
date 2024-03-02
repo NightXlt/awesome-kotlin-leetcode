7b1fa5e65c2676b87dea4127e5c1b6adf6f9af7a
night <nightfeng26@gmail.com>
2024-03-02 19:45:04 +0800

    opt naming

--- src/main/java/com/blankj/medium/_427/Solution.kt
+++ src/main/java/com/blankj/medium/_427/Solution.kt
@@ -29,19 +29,19 @@ class Solution {
         return dfs(0, 0, grid.lastIndex, grid.lastIndex)
     }
 
-    private fun dfs(row1: Int, col1: Int, row2: Int, col2: Int): Node? {
-        val curSum = sum[row2 + 1][col2 + 1] + sum[row1][col1] - sum[row2 + 1][col1] - sum[row1][col2 + 1]
-        val dx = row2 - row1 + 1
-        val dy = col2 - col1 + 1
+    private fun dfs(x1: Int, y1: Int, x2: Int, y2: Int): Node? {
+        val curSum = sum[x2 + 1][y2 + 1] + sum[x1][y1] - sum[x2 + 1][y1] - sum[x1][y2 + 1]
+        val dx = x2 - x1 + 1
+        val dy = y2 - y1 + 1
         val total = dx * dy
         if (curSum == 0 || curSum == total) {
             return Node(curSum == total, true)
         }
-        val node = Node(g[row1][col1] == 0, false)
-        node.topLeft = dfs(row1, col1, row1 + dx / 2 - 1, col1 + dy / 2 - 1)
-        node.topRight = dfs(row1, col1 + dy / 2, row1 + dx / 2 - 1, col2)
-        node.bottomLeft = dfs(row1 + dx / 2, col1, row2, col1 + dy / 2 - 1)
-        node.bottomRight = dfs(row1 + dx / 2, col1 + dy / 2, row2, col2)
+        val node = Node(g[x1][y1] == 0, false)
+        node.topLeft = dfs(x1, y1, x1 + dx / 2 - 1, y1 + dy / 2 - 1)
+        node.topRight = dfs(x1, y1 + dy / 2, x1 + dx / 2 - 1, y2)
+        node.bottomLeft = dfs(x1 + dx / 2, y1, x2, y1 + dy / 2 - 1)
+        node.bottomRight = dfs(x1 + dx / 2, y1 + dy / 2, x2, y2)
         return node
     }
 }
