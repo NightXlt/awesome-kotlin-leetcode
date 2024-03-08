9485836395683a01f310badf3ed768c388c1155b
night <nightfeng26@gmail.com>
2024-03-08 13:42:49 +0800

    more concise code

--- src/main/java/com/blankj/medium/_213/Solution.kt
+++ src/main/java/com/blankj/medium/_213/Solution.kt
@@ -6,23 +6,18 @@ import kotlin.math.max
 class Solution {
     fun rob(nums: IntArray): Int {
         if (nums.isEmpty()) return 0
-        if (nums.size == 1) {
-            return nums[0];
-        } else if (nums.size == 2) {
-            return max(nums[0], nums[1]);
-        }
+        if (nums.size <= 2) return nums.max()
         return max(
             helper(nums, 0, nums.size - 2),
             helper(nums, 1, nums.size - 1)
         )
     }
-
-    fun helper(nums: IntArray, start: Int, end: Int): Int {
+    private fun helper(nums: IntArray, start: Int, end: Int): Int {
         var first = nums[start]
-        var second = max(nums[start], nums[end])
-        for (i in start + 2..end) {
+        var second = max(nums[start], nums[start + 1])
+        for (i in start+2..end) {
             val temp = second
-            second = max(second, first + nums[i])
+            second = max(first + nums[i], second)
             first = temp
         }
         return second
