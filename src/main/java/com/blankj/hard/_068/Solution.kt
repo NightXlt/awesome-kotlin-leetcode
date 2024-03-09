5b9f3461317b992f806c16d23f56c538d761a700
night <nightfeng26@gmail.com>
2024-03-09 12:25:07 +0800

    opt code

--- src/main/java/com/blankj/hard/_068/Solution.kt
+++ src/main/java/com/blankj/hard/_068/Solution.kt
@@ -1,6 +1,7 @@
 package com.blankj.hard._068
 
 import com.blankj.ext.print
+import java.lang.Appendable
 
 class Solution {
     fun fullJustify(words: Array<String>, maxWidth: Int): List<String> {
@@ -14,7 +15,7 @@ class Solution {
                 sumLength += words[right++].length
             }
             if (right == words.size) {
-                val lastLine = join(words, left, words.size, " ")
+                val lastLine = StringBuilder().join(words, left, words.size, " ")
                 lastLine.append(" ".repeat(maxWidth - lastLine.length))
                 res.add(lastLine.toString())
                 return res
@@ -32,21 +33,21 @@ class Solution {
             val avgSpaces = numSpaces / (numWords - 1)
             val countWithextraSpaces = numSpaces % (numWords - 1)
             res.add(buildString {
-                append(join(words, left, left + countWithextraSpaces + 1, " ".repeat(avgSpaces + 1)))
+                join(words, left, left + countWithextraSpaces + 1, " ".repeat(avgSpaces + 1))
                 append(" ".repeat(avgSpaces))
-                append(join(words, left + countWithextraSpaces + 1, right, " ".repeat(avgSpaces)))
+                join(words, left + countWithextraSpaces + 1, right, " ".repeat(avgSpaces))
             })
         }
     }
 
     // Join string from left to (right - 1)
-    private fun join(words: Array<String>, left: Int, right: Int, separator: String): StringBuilder {
-        val builder = StringBuilder(words[left])
+    private fun StringBuilder.join(words: Array<String>, left: Int, right: Int, separator: String): StringBuilder {
+        append(words[left])
         for (i in left + 1..<right) {
-            builder.append(separator)
-            builder.append(words[i])
+            append(separator)
+            append(words[i])
         }
-        return builder
+        return this
     }
 }
 
